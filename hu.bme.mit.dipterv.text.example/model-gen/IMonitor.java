3ea0c35f38585e40b24d6ff3ab2884b0b89244ac
BakaiIstvan <bakai.istvan.lfgeb@gmail.com>
2021-05-06 16:57:33 +0200

    Added example maven project with dsl as maven-xtext-plugin

---
+++ hu.bme.mit.dipterv.text.example/model-gen/IMonitor.java
@@ -0,0 +1,6 @@
+public interface IMonitor {
+	public boolean goodStateReached();
+	public void update(String sender, String receiver, String messageType, String[] parameters);
+	public boolean requirementSatisfied();
+	public void errorDetected(String sender, String receiver, String messageType, String[] parameters);
+}
