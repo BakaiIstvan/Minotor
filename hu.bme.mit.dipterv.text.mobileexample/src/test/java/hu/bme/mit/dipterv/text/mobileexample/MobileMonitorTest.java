61bd9bbe6db65dd821b61cd534b0fde65ef86f52
BakaiIstvan <bakai.istvan.lfgeb@gmail.com>
2021-11-02 16:50:44 +0100

    Added testcase for simple message not triggering scenario

--- hu.bme.mit.dipterv.text.mobileexample/src/test/java/hu/bme/mit/dipterv/text/mobileexample/MobileMonitorTest.java
+++ hu.bme.mit.dipterv.text.mobileexample/src/test/java/hu/bme/mit/dipterv/text/mobileexample/MobileMonitorTest.java
@@ -38,6 +38,28 @@ public class MobileMonitorTest implements ISystem {
 		Assertions.assertTrue(requirementSatisfied);
 		Assertions.assertFalse(errorDetected);
 	}
+
+	@Test
+	public void testMobileScenarioNeverStarts() {
+		resetValues();
+		Specification specification = new Specification();
+		specification.listAutomatas();
+		IClock clock = new Clock();
+		IMonitor monitor = new Monitor(specification.getAutomata().get(0), clock, this);
+		
+		monitor.update("user", "device", "randomMessage", new String[] {}, true);
+		monitor.update("device", "device", "accessFile", new String[] {}, true);
+		monitor.update("device", "user", "getMusic", new String[] {}, true);
+		monitor.update("someone", "else", "message", new String[] {}, true);
+		monitor.update("device", "db", "retrieveData", new String[] {}, true);
+		monitor.update("device", "db", "retrieveInformation", new String[] {}, true);
+		monitor.update("db", "device", "generateDocuments", new String[] {}, true);
+
+		Assertions.assertTrue(monitor.goodStateReached());
+		Assertions.assertFalse(monitor.requirementSatisfied());
+		Assertions.assertFalse(requirementSatisfied);
+		Assertions.assertFalse(errorDetected);
+	}
 	
 	@Test
 	public void testMobileWithError() {
