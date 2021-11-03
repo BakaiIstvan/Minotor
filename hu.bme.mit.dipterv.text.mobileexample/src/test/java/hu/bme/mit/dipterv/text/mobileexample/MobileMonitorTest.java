be371d29e4c29ec2927150519128b7c8f8c5f536
BakaiIstvan <bakai.istvan.lfgeb@gmail.com>
2021-11-03 11:09:30 +0100

    Added testcase for future constraint

--- hu.bme.mit.dipterv.text.mobileexample/src/test/java/hu/bme/mit/dipterv/text/mobileexample/MobileMonitorTest.java
+++ hu.bme.mit.dipterv.text.mobileexample/src/test/java/hu/bme/mit/dipterv/text/mobileexample/MobileMonitorTest.java
@@ -38,6 +38,52 @@ public class MobileMonitorTest implements ISystem {
 		Assertions.assertTrue(requirementSatisfied);
 		Assertions.assertFalse(errorDetected);
 	}
+
+	@Test
+	public void testMobileFutureConstraint() {
+		resetValues();
+		Specification specification = new Specification();
+		specification.listAutomatas();
+		IClock clock = new Clock();
+		IMonitor monitor = new Monitor(specification.getAutomata().get(0), clock, this);
+		
+		monitor.update("user", "device", "openApp", new String[] {}, true);
+		monitor.update("device", "device", "accessWebcam", new String[] {}, true);
+		monitor.update("device", "user", "getPhoto", new String[] {}, true);
+		monitor.update("user", "device", "closeApp", new String[] {}, true);
+		monitor.update("someone", "else", "message", new String[] {}, true);
+		monitor.update("device", "db", "retrieveMood", new String[] {}, true);
+		monitor.update("device", "db", "retrieveMusic", new String[] {}, true);
+		monitor.update("db", "device", "generatePlaylist", new String[] {}, true);
+
+		Assertions.assertFalse(monitor.goodStateReached());
+		Assertions.assertFalse(monitor.requirementSatisfied());
+		Assertions.assertFalse(requirementSatisfied);
+		Assertions.assertTrue(errorDetected);
+	}
+
+	@Test
+	public void testMobileFutureConstraintEarly() {
+		resetValues();
+		Specification specification = new Specification();
+		specification.listAutomatas();
+		IClock clock = new Clock();
+		IMonitor monitor = new Monitor(specification.getAutomata().get(0), clock, this);
+		
+		monitor.update("user", "device", "closeApp", new String[] {}, true);
+		monitor.update("user", "device", "openApp", new String[] {}, true);
+		monitor.update("device", "device", "accessWebcam", new String[] {}, true);
+		monitor.update("device", "user", "getPhoto", new String[] {}, true);
+		monitor.update("someone", "else", "message", new String[] {}, true);
+		monitor.update("device", "db", "retrieveMood", new String[] {}, true);
+		monitor.update("device", "db", "retrieveMusic", new String[] {}, true);
+		monitor.update("db", "device", "generatePlaylist", new String[] {}, true);
+
+		Assertions.assertFalse(monitor.goodStateReached());
+		Assertions.assertFalse(monitor.requirementSatisfied());
+		Assertions.assertFalse(requirementSatisfied);
+		Assertions.assertTrue(errorDetected);
+	}
 	
 	@Test
 	public void testMobileWithError() {
