3ddcca00c5b9ea0cf7f207e09d86256e6da23be5
BakaiIstvan <bakai.istvan.lfgeb@gmail.com>
2021-11-28 14:56:15 +0100

    Added second gamma project

---
+++ hu.bme.mit.dipterv.text.gammakomplexexample/src/test/java/hu/bme/mit/dipterv/text/gammakomplexexample/GammaMonitorTest.java
@@ -0,0 +1,73 @@
+package hu.bme.mit.dipterv.text.gammakomplexexample;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import hu.bme.mit.gamma.tutorial.extra.VirtualTimerService;
+import hu.bme.mit.gamma.tutorial.extra.monitoredcrossroad.ReflectiveMonitoredCrossroad;
+import util.ISystem;
+
+public class GammaMonitorTest implements ISystem {
+	private boolean requirementSatisfied = false;
+	private boolean errorDetected = false;
+	
+	private static ReflectiveMonitoredCrossroad reflectiveMonitoredCrossroad;
+	private static VirtualTimerService timer;
+
+	//TODO: use @BeforeEach, didn't work for some reason last time
+	public void resetValues() {
+		requirementSatisfied = false;
+		errorDetected = false;
+		timer = new VirtualTimerService();
+		reflectiveMonitoredCrossroad = new ReflectiveMonitoredCrossroad(timer, this);  // Virtual timer is automatically set
+		System.out.println("[GammaMonitorTest] Resetting values");
+	}
+	
+	public void tearDown() {
+		stop();
+	}
+	
+	// Only for override by potential subclasses
+	protected void stop() {
+		timer = null;
+		reflectiveMonitoredCrossroad = null;				
+	}
+
+	@Test
+	public void testNetworkRequirementSatisfied() {
+		resetValues();
+		
+		timer.reset(); // Timer before the system
+		reflectiveMonitoredCrossroad.reset();
+		
+		timer.elapse(2000);
+		reflectiveMonitoredCrossroad.schedule(null);
+		
+		timer.elapse(2000);
+		reflectiveMonitoredCrossroad.schedule(null);
+		
+		Assertions.assertTrue(reflectiveMonitoredCrossroad.getWrappedComponent().getMonitor().goodStateReached());
+		Assertions.assertTrue(reflectiveMonitoredCrossroad.getWrappedComponent().getMonitor().requirementSatisfied());
+		Assertions.assertTrue(requirementSatisfied);
+		Assertions.assertFalse(errorDetected);
+		tearDown();
+	}
+
+	@Override
+	public void receiveMonitorStatus(String message) {
+		System.out.println("[GammaMonitorTest] Received status from monitor: " + message);
+	}
+
+	@Override
+	public void receiveMonitorError(String actualMessage, String lastAcceptedMessage) {
+		System.out.println("[GammaMonitorTest] Received error report from Monitor for " + actualMessage + " message.");
+		System.out.println("Last accepted message was: " + lastAcceptedMessage);
+		errorDetected = true;
+	}
+
+	@Override
+	public void receiveMonitorSuccess() {
+		System.out.println("[GammaMonitorTest] Monitor reported that the requirement was satisfied");
+		requirementSatisfied = true;
+	}
+}
